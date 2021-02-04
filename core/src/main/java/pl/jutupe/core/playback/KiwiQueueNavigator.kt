2a126370d5d0d21da70d5db0364385f271ae0ad6
Wiktor Petryszyn <jutuper@gmail.com>
2021-02-04 18:33:36 +0100

    Queue core preparations  + valid queue is now returned from service  - SquareLayout replaced with constraints

--- core/src/main/java/pl/jutupe/core/playback/KiwiQueueNavigator.kt
+++ core/src/main/java/pl/jutupe/core/playback/KiwiQueueNavigator.kt
@@ -3,15 +3,16 @@ package pl.jutupe.core.playback
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.session.MediaSessionCompat
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Timeline
 import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator
 
 class KiwiQueueNavigator(
     mediaSession: MediaSessionCompat
-) : TimelineQueueNavigator(mediaSession) {
+) : TimelineQueueNavigator(mediaSession, 50) {
 
     override fun getMediaDescription(
         player: Player,
         windowIndex: Int
     ): MediaDescriptionCompat =
-        player.currentMediaItem?.playbackProperties?.tag as MediaDescriptionCompat
+        player.currentTimeline.getWindow(windowIndex, Timeline.Window()).mediaItem.playbackProperties?.tag as MediaDescriptionCompat
 }
