ee8a7fac391d6cef00cfcf1714472cf9c1da0e0a
Wiktor Petryszyn <petryszyn@o2.pl>
2021-06-28 19:33:31 +0200

    :bug: Coil performance fix

--- commons/ui/src/main/java/pl/jutupe/ui/util/ImageBinding.kt
+++ commons/ui/src/main/java/pl/jutupe/ui/util/ImageBinding.kt
@@ -3,6 +3,7 @@ package pl.jutupe.ui.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
+import coil.size.Precision
 import pl.jutupe.model.MediaItem
 
 @BindingAdapter("mediaImage")
@@ -12,6 +13,8 @@ fun ImageView.bindMediaImage(item: MediaItem?) {
     load(item.art) {
         val baseDrawable = item.type.getItemBaseDrawable()
         memoryCacheKey(item.type.name + item.art)
+        precision(Precision.INEXACT)
+        runCatching { size(width / 2, height / 2) }
         error(baseDrawable)
         placeholder(baseDrawable)
     }
